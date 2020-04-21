04c038052f830919fb5719152ab8897d2afdd139
Alex Nail Quiroz Palma <al221810641@gmail.com>
2020-04-21 18:32:47 -0500

    Fin de Clase App

--- src/App.js
+++ src/App.js
@@ -4,34 +4,22 @@ import './App.css';
 import App from './App';
 
 class App extends Component{
-constructor(){
-super();
-
-this.state = {
-  names:[]
-};
-
-axios.get("http://localhost:3001/names")
-.then(response =>{
-  this.setState({
-    names: response.data
-})
-})
-.catch(error=> {
-  console.log(error);
-})
-}
-
-render(){
-  return(
-    <div>
-        <h1>Hola mundo</h1>
-          <ul>{this.state.names.map(name =>
-  <li key={name}>{name}</li>
-  )}
-  </ul>
-  </div>
-  );
-}
-}
-export default App;
+  constructor(){
+  super();
+  
+  this.state = {
+    names:[]
+  };
+  
+  axios.get("http://localhost:3001/names")
+  .then(response =>{
+    this.setState({
+      names: response.data
+  })
+  })
+  .catch(error=> {
+    console.log(error);
+  })
+  }
+  
+  
