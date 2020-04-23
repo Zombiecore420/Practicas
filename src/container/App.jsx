da3295645eb77752961b9ccd7d0a23bba6cefeae
Alex Nail Quiroz Palma <al221810641@gmail.com>
2020-04-23 18:09:21 -0500

    importación de componentes

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Footer from '../components/Footer';
-
+import Carrusel from '../components/carrusel';
+import Carruselitem from '../components/carruselitem';
+import Categoria from '../components/categoria';
 
 import '../assets/style/Header.scss';
 
@@ -13,8 +15,13 @@ const App= () =>{
         <div className="App">
           <Header />
           <Search />
+          <Categoria/>
+          <Carruselitem/>
+          <Carruselitem/>
+          <Carruselitem/>
+          <Carruselitem/>
+          <Carrusel/>
           
-  
           
           <Footer />
          
